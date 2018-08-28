2054ed5c99b407e66826efe0d88abf914affa274
luchoss10 <lucncd@gmail.com>
2018-08-28 17:48:07 -0500

    Control Sensor actualizado
    Establecidos los ejes y movmientos basicos.

--- Prueba2.cs
+++ Prueba2.cs
@@ -11,6 +11,7 @@ public class Prueba2 : MonoBehaviour
 
     public GameObject target; // is the gameobject to 
 
+
     float acc_normalizer_factor = 0.00025f;
     float gyro_normalizer_factor = 1.0f / 32768.0f;   // 32768 is max value captured during test on imu
 
@@ -26,8 +27,27 @@ public class Prueba2 : MonoBehaviour
     float A_R = 16384.0f;
     float G_R = 131.0f;
 
+    float ax;
+    float ay;
+    float az;
+    float gx;
+    float gy;
+    float gz;
+
+    public float ax2;
+    public float ay2;
+    float ax21;
+    float ay21;
+    float az2;
+    float gz2;
+
+
+    float ro = 0;
+    float phi = 0;
+
     // Increase the speed/influence rotation
     public float factor = 7;
+    public float speed = 4.0f;
 
 
     public bool enableRotation;
@@ -36,7 +56,7 @@ public class Prueba2 : MonoBehaviour
     // SELECT YOUR COM PORT AND BAUDRATE
     string port = "COM8";
     int baudrate = 9600;
-    int readTimeout = 25;
+    int readTimeout = 10;
 
     void Start()
     {
@@ -83,68 +103,59 @@ public class Prueba2 : MonoBehaviour
             string[] dataRaw = dataString.Split(splitChar);
 
             // normalized accelerometer values
-            float ax = Int32.Parse(dataRaw[0]) / A_R; //* acc_normalizer_factor;
-            float ay = Int32.Parse(dataRaw[1]) / A_R; //* acc_normalizer_factor;
-            float az = Int32.Parse(dataRaw[2]) / A_R; //* acc_normalizer_factor;
+            ax = Int32.Parse(dataRaw[0]) / A_R; //* acc_normalizer_factor;
+            ay = Int32.Parse(dataRaw[1]) / A_R; //* acc_normalizer_factor;
+            az = Int32.Parse(dataRaw[2]) / A_R; //* acc_normalizer_factor;
 
-            //Debug.Log(az);
             // normalized gyrocope values
-            float gx = Int32.Parse(dataRaw[3]) / G_R; //* gyro_normalizer_factor;
-            float gy = Int32.Parse(dataRaw[4]) / G_R; //* gyro_normalizer_factor;
-            float gz = Int32.Parse(dataRaw[5]) / G_R; //* gyro_normalizer_factor;
+            gx = Int32.Parse(dataRaw[3]) / G_R; //* gyro_normalizer_factor;
+            gy = Int32.Parse(dataRaw[4]) / G_R; //* gyro_normalizer_factor;
+            gz = Int32.Parse(dataRaw[5]) / G_R; //* gyro_normalizer_factor;
 
-            float az2 = az * A_R * acc_normalizer_factor;
-            float gz2 = gz * G_R * gyro_normalizer_factor;
+            ro = (float)Math.Atan((ax) / (Math.Sqrt(Math.Pow(ay, 2) + Math.Pow(az, 2)))) * RAD_A_DEG;//Calcular grados con aceleraciones se calculan en rad se cambian a Dregez
+            phi = (float)Math.Atan((ay) / (Math.Sqrt(Math.Pow(ax, 2) + Math.Pow(az, 2)))) * RAD_A_DEG;
 
-            // prevent 
-            //if (Mathf.Abs(ax) - 1 < 0) ax = 0;
-            //if (Mathf.Abs(ay) - 1 < 0) ay = 0;
-            if (Mathf.Abs(az2) - 1 < 0) az2 = 0;
+        }
 
+    }
 
 
-            //curr_offset_x += ax;
-            //curr_offset_y += ay;
-            curr_offset_z += 0; // The IMU module have value of z axis of 16600 caused by gravity
+    private void FixedUpdate()
+    {
+        az2 = az * A_R * acc_normalizer_factor;
+        ax2 = ax * A_R * acc_normalizer_factor;
+        ay2 = ay * A_R * acc_normalizer_factor;
+        gz2 = gz * G_R * gyro_normalizer_factor;
 
+        if (ax2 < 0.5f && ax2 > -0.5f) ax2 = 0.0f;
+        if (ay2 < 0.5f && ay2 > -0.5f) ay2 = 0.0f;
 
-            // prevent little noise effect
-            //if (Mathf.Abs(gx) < 0.025f) gx = 0f;
-            //if (Mathf.Abs(gy) < 0.025f) gy = 0f;
-            if (Mathf.Abs(gz2) < 0.025f) gz2 = 0f;
+        ax21 += ax2 ;
+        ay21 += ay2 ;
 
-            //curr_angle_x += gx;
-            //curr_angle_y += gy;
-            curr_angle_z += gz2;
+        if (ax21 < 0.5f && ax21 > -0.5f) ax21 = 0.0f;
+        if (ay21 < 0.5f && ay21 > -0.5f) ay21 = 0.0f;
 
-            //Debug.Log(ax+","+ay+","+az);
+        // prevent 
+        if (Mathf.Abs(az2) - 1 < 0) az2 = 0;
 
+        curr_offset_z += 0; // The IMU module have value of z axis of 16600 caused by gravity
 
-            float ro = (float) Math.Atan((ax)/(Math.Sqrt(Math.Pow(ay,2) + Math.Pow(az,2)))) * RAD_A_DEG;//Calcular grados con aceleraciones se calculan en rad se cambian a Dregez
-            float phi = (float)Math.Atan((ay)/(Math.Sqrt(Math.Pow(ax, 2) + Math.Pow(az, 2)))) * RAD_A_DEG;
-            //float teta = (float)Math.Atan((Math.Sqrt(Math.Pow(ax, 2) + Math.Pow(ay, 2)))/(az)) * RAD_A_DEG;
+        // prevent little noise effect
+        //if (Mathf.Abs(gz2) < 0.025f) gz2 = 0f;
 
-            //Debug.Log(ro + "," + phi + "," + teta);
+        //curr_angle_z += gz2;
 
-            curr_angle_x = 0.96f * (curr_angle_x + gx * 0.025f) + 0.04f * ro;
-            //Debug.Log(curr_angle_x);
-            curr_angle_y = 0.96f * (curr_angle_y + gy * 0.025f) + 0.04f * phi;
-            //curr_angle_z = 0.99f * (curr_angle_z + gz * Time.deltaTime) + 0.01f * teta;
+        //Filtro complementario
+        curr_angle_x = 0.96f * (curr_angle_x + gx * 0.005f) + 0.04f * ro * -1.0f;//*-1 YA QUE LOS VALORES CON RESPECTO A MI CARA DE REFERENCIA SON ESOS
+        curr_angle_y = 0.96f * (curr_angle_y + gy * 0.005f) + 0.04f * phi * -1.0f;
 
+        //Vector3 angles = new Vector3(curr_angle_x, curr_angle_z * factor * -1.0f, curr_angle_y);
+        Vector3 angles = new Vector3(curr_angle_x, 0, curr_angle_y);
+        Vector3 acelerations = new Vector3(ay21, 0, ax21);
 
-            Debug.Log(curr_angle_x + "," + curr_angle_y * factor + "," + curr_angle_z);
-
-            Vector3 angles = new Vector3(curr_angle_x, curr_angle_z * factor, -1 * curr_angle_y);
-
-            if (enableTranslation) target.transform.position = new Vector3(curr_offset_x, curr_offset_z, curr_offset_y);
-            if (enableRotation) target.transform.localRotation = Quaternion.Euler(angles);
-            /*if (enableRotation)
-            {
-                target.transform.Rotate(Vector3.up, curr_angle_z * Time.deltaTime);
-                target.transform.Rotate(Vector3.right, curr_angle_x * Time.deltaTime);
-                target.transform.Rotate(Vector3.back, curr_angle_z * Time.deltaTime);
-            }*/
-            }
+        if (enableTranslation) target.transform.position = acelerations * speed * Time.deltaTime;
+        if (enableRotation) target.transform.localRotation = Quaternion.Euler(angles);
     }
 
 }
